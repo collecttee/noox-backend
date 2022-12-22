355c952b9cdbbe1aef82b3a17f76f7ac82131138
sss <sss>
2022-12-22 17:46:31 +0800

    a

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -1,26 +1,17 @@
 <?php
 namespace App\Http\Controllers;
-use         kornrunner\Keccak;
-use         Web3\Utils;
-use         Web3\Contracts\Ethabi;
-use         Web3\Contracts\Types\Address;
-use         Web3\Contracts\Types\Boolean;
-use         Web3\Contracts\Types\Bytes;
-use         Web3\Contracts\Types\DynamicBytes;
-use         Web3\Contracts\Types\Integer;
-use         Web3\Contracts\Types\Str;
-use         Web3\Contracts\Types\Uinteger;
+use App\Resources\EthSign;
 class TestController extends Controller {
-    public function test(){
-        $abi = new Ethabi([
-            'address' => new Address,
-            'bool'  => new Boolean,
-            'bytes' => new Bytes,
-            'dynamicBytes'  => new DynamicBytes,
-            'int' => new Integer,
-            'string' => new Str,
-            'uint'  => new Uinteger
-        ]);
-       echo $abi->encodeParameter('address','0xC97097713279324bC626F2Ef4730277c136598e0');
+    const _CLAIM_TYPEHASH = '0x9e1b5a33d73aa59c05b5f14aacea44a5012466445786e62e35d3bb907d627659';
+    const TO = '0xC97097713279324bC626F2Ef4730277c136598e0';
+    protected $private = '93f78d7bd7ef27829f4f2f06d77d057d05f1c5a32bade613a8a73088584795fc';
+    public function test() {
+        $types = ['address','uint'];
+        $values = [self::TO,1];
+        $a = EthSign::sha3AbiEncode($types,$values);
+        echo $a.PHP_EOL;
+        $sign = new EthSign($this->private);
+      echo  $sign->signMessage($a);
+
     }
 }
---
+++ app/Resources/EthSign.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\Resources;
+use kornrunner\Keccak;
+use Web3\Utils;
+use Web3\Contracts\Ethabi;
+use Web3\Contracts\Types\Address;
+use Web3\Contracts\Types\Boolean;
+use Web3\Contracts\Types\Bytes;
+use Web3\Contracts\Types\DynamicBytes;
+use Web3\Contracts\Types\Integer;
+use Web3\Contracts\Types\Str;
+use Web3\Contracts\Types\Uinteger;
+use Elliptic\EC;
+class EthSign{
+    private  $ec;
+    private $privateKey;
+    public function __construct($privateKey = '')
+    {
+        $this->privateKey = $privateKey;
+        $this->ec = new EC('secp256k1');
+    }
+    public static function sha3AbiEncode($types,$values){
+        $abi = new Ethabi([
+            'address' => new Address,
+            'bool'  => new Boolean,
+            'bytes' => new Bytes,
+            'dynamicBytes'  => new DynamicBytes,
+            'int' => new Integer,
+            'string' => new Str,
+            'uint'  => new Uinteger
+        ]);
+        return Utils::sha3($abi->encodeParameters($types,$values));
+    }
+    public function signMessage(string $message): string
+    {
+//        $message = "\x19Ethereum Signed Message:\n" . strlen($message) . $message;
+        $message = strlen($message) . $message;
+        $hash = Keccak::hash($message, 256);
+
+        $ecPrivateKey = $this->ec->keyFromPrivate($this->privateKey, 'hex');
+        $signature = $ecPrivateKey->sign($hash, ['canonical' => true]);
+
+        $r = str_pad($signature->r->toString(16), 64, '0', STR_PAD_LEFT);
+        $s = str_pad($signature->s->toString(16), 64, '0', STR_PAD_LEFT);
+        $v = dechex($signature->recoveryParam);
+
+        return '0x' . $r . $s . 0 . $v;
+    }
+
+}
